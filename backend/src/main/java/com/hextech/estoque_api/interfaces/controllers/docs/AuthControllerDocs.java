034e19bb96c568c012debd81efbfeb5ed767d988
Nathangc77 <nathangc7@gmail.com>
2025-09-09 11:55:44 -0300

    feat: implementar documentacao para todos os endpoits da API com suporte a JWT

---
+++ backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/docs/AuthControllerDocs.java
@@ -0,0 +1,22 @@
+package com.hextech.estoque_api.interfaces.controllers.docs;
+
+import com.hextech.estoque_api.application.dtos.security.AccountCredentialsDTO;
+import com.hextech.estoque_api.application.dtos.security.TokenDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Autenticação", description = "Operações de Autenticação")
+public interface AuthControllerDocs {
+
+    @Operation(summary = "Realiza o login de um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = TokenDTO.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
+    ResponseEntity<?> login(AccountCredentialsDTO credentials);
+}
---
+++ backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/docs/MovementControllerDocs.java
@@ -0,0 +1,35 @@
+package com.hextech.estoque_api.interfaces.controllers.docs;
+
+import com.hextech.estoque_api.application.dtos.movements.MovementRequestDTO;
+import com.hextech.estoque_api.application.dtos.movements.MovementResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Tag(name = "Movimentações", description = "Operações de Movimentações")
+public interface MovementControllerDocs {
+
+    @Operation(summary = "Gera um relatório de movimentações filtrando por data de início e fim (opcional), formato aceito: yyyy-MM-dd")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+    })
+    ResponseEntity<List<MovementResponseDTO>> reportMovements(String startDate, String endDate);
+
+    @Operation(summary = "Cria uma nova movimentação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found"),
+            @ApiResponse(responseCode = "422", description = "Unprocessable Entity")
+    })
+    ResponseEntity<MovementResponseDTO> createMovement(MovementRequestDTO requestDTO);
+}
