deffd377699756465c03a1aa9afabfd7352a40e6
Nathangc77 <nathangc7@gmail.com>
2025-09-10 12:15:32 -0300

    docs: adicionar documentacao para o endpoint de criacao de usuario

--- backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/UserController.java
+++ backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.hextech.estoque_api.application.dtos.users.UserRequestDTO;
 import com.hextech.estoque_api.application.dtos.users.UserResponseDTO;
 import com.hextech.estoque_api.application.security.AuthContext;
 import com.hextech.estoque_api.application.services.UserService;
+import com.hextech.estoque_api.interfaces.controllers.docs.UserControllerDocs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-public class UserController {
+@RequestMapping(value = "/api/users", produces = "application/json")
+public class UserController implements UserControllerDocs {
 
     @Autowired
     private UserService service;
