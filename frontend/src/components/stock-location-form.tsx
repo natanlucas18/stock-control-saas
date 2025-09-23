17b9243d88c6e7811bc45a57f2c7b705159a6776
Natan Lucas <167147473+natanlucas18@users.noreply.github.com>
2025-09-23 18:52:41 -0300

    adiciona formulario de local de estoque

---
+++ frontend/src/components/stock-location-form.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { createStockLocations } from '@/app/requests/stock-location-request';
+import { stockLocationsFormSchema, StockLocationsFormType } from '@/types/stock-location-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { Button } from './ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from './ui/form';
+import { Input } from './ui/input';
+
+export default function StockLocationForm() {
+  const hookForm = useForm<StockLocationsFormType>({
+    resolver: zodResolver(stockLocationsFormSchema),
+    defaultValues: {
+      name: '',
+    }
+  });
+
+  const onSubmit = async (data: StockLocationsFormType) => {
+    const result = await createStockLocations(data);
+    console.log(result);
+
+    if (result.success) toast.success('Local de estoque cadastrado com sucesso!');
+
+    if (!result.success) toast.error('Erro ao cadastrar local de estoque!');
+  };
+
+  return (
+    <div>
+      <Form {...hookForm}>
+        <form
+          onSubmit={hookForm.handleSubmit(onSubmit)}
+          className='space-y-8 border p-5 rounded-md'
+        >
+          <FormField
+            control={hookForm.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input
+                    type='text'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type='submit'>Cadastrar</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
