faa18085557856828071b5a68cae52a67cd973d7
Nathangc77 <nathangc7@gmail.com>
2025-09-25 10:05:38 -0300

    chore: adiciona novos inserts nas migrations para testes no banco de dados

--- backend/src/main/resources/db/migration/V11__insert_join_users_roles.sql
+++ backend/src/main/resources/db/migration/V11__insert_join_users_roles.sql
@@ -1,5 +1,7 @@
 INSERT INTO user_role (user_id, role_id)
 VALUES (1, 1),
+       (1, 2),
        (2, 1),
-       (3, 2),
-       (4, 2);
+       (3, 1),
+       (4, 2),
+       (5, 2);
--- backend/src/main/resources/db/migration/V9__insert_users.sql
+++ backend/src/main/resources/db/migration/V9__insert_users.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (name, email, password, company_id, enabled)
 VALUES ('Nathan Moreira', 'nathan@gmail.com', '{pbkdf2}2d8e93af5a655ab01ad676fe44cffb9f1fb23dec17ff6b97b6491428bb785c1963f69959dce30e90', 1, true),
        ('Natan Lucas', 'natan@gmail.com', '{pbkdf2}2d8e93af5a655ab01ad676fe44cffb9f1fb23dec17ff6b97b6491428bb785c1963f69959dce30e90', 1, true),
+       ('Alisson Silva', 'alisson@gmail.com', '{pbkdf2}2d8e93af5a655ab01ad676fe44cffb9f1fb23dec17ff6b97b6491428bb785c1963f69959dce30e90', 1, true),
          ('José Carlos', 'jose@gmail.com', '{pbkdf2}2d8e93af5a655ab01ad676fe44cffb9f1fb23dec17ff6b97b6491428bb785c1963f69959dce30e90', 2, true),
          ('Maria Silva', 'maria@gmail.com', '{pbkdf2}2d8e93af5a655ab01ad676fe44cffb9f1fb23dec17ff6b97b6491428bb785c1963f69959dce30e90', 3, true);
