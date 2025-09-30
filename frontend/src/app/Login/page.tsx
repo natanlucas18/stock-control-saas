a8073317cbfbe81d3740c6ac2af7c4d948214767
alissonasj <alissonsilva.jesus@gmail.com>
2025-09-29 22:53:27 -0300

    concerta erro de autenticacao e adiciona funcao de request

--- frontend/src/app/Login/page.tsx
+++ frontend/src/app/Login/page.tsx
@@ -1,88 +1,104 @@
-"use client";
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { signIn } from 'next-auth/react';
+'use client';
 import { FormLogin } from '@/types/types';
-
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signIn } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 const schema = z.object({
   username: z.email('Email inválido').min(1, 'Insira seu email'),
-  password: z.string().min(1, 'Insira sua senha'),
+  password: z.string().min(1, 'Insira sua senha')
 });
 
 export default function Login() {
-  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<FormLogin>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<FormLogin>({
     resolver: zodResolver(schema)
   });
 
-
   const onSubmit = async (data: FormLogin) => {
-    signIn("credentials", {
+    signIn('credentials', {
       ...data,
-      callbackUrl: "/Home",
+      callbackUrl: '/Home'
     });
   };
   return (
     <>
-      <div className="flex min-h-full h-screen bg-black flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className='flex min-h-full h-screen bg-black flex-col justify-center px-6 py-12 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
           <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white"
-            className="mx-auto h-10 w-auto"
+            alt='Your Company'
+            src='https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white'
+            className='mx-auto h-10 w-auto'
           />
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Faça Login com Sua Conta!</h2>
+          <h2 className='mt-10 text-center text-2xl/9 font-bold tracking-tight text-white'>
+            Faça Login com Sua Conta!
+          </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='space-y-6'
+          >
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
+              <label
+                htmlFor='email'
+                className='block text-sm/6 font-medium text-gray-100'
+              >
                 Email
               </label>
-              <div className="mt-2">
+              <div className='mt-2'>
                 <input
-                  id="email"
-                  type="email"
+                  id='email'
+                  type='email'
                   required
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-800 sm:text-sm/6"
-                  {...register("username")}
+                  autoComplete='email'
+                  className='block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-800 sm:text-sm/6'
+                  {...register('username')}
                 />
                 {errors.username && (
-                  <span className='text-red-500 text-sm'>{errors.username.message}</span>
+                  <span className='text-red-500 text-sm'>
+                    {errors.username.message}
+                  </span>
                 )}
               </div>
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
+              <div className='flex items-center justify-between'>
+                <label
+                  htmlFor='password'
+                  className='block text-sm/6 font-medium text-gray-100'
+                >
                   Senha
                 </label>
               </div>
-              <div className="mt-2">
+              <div className='mt-2'>
                 <input
-                  id="password"
-                  type="password"
+                  id='password'
+                  type='password'
                   required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-800 sm:text-sm/6"
-                  {...register("password")}
+                  autoComplete='current-password'
+                  className='block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-800 sm:text-sm/6'
+                  {...register('password')}
                 />
                 {errors.password && (
-                  <span className='text-red-500 text-sm'>{errors.password.message}</span>
+                  <span className='text-red-500 text-sm'>
+                    {errors.password.message}
+                  </span>
                 )}
               </div>
             </div>
 
             <div>
               <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-blue-800 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-blue-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                >
+                type='submit'
+                className='flex w-full justify-center rounded-md bg-blue-800 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-blue-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+              >
                 {isSubmitting ? 'Logando...' : 'Entrar'}
               </button>
             </div>
--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,47 @@
-import { AuthData } from '@/types/types';
+import { login } from '@/app/requests/login-request';
 import NextAuth from 'next-auth';
-import  CredentialsProvider  from 'next-auth/providers/credentials';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { cookies } from 'next/headers';
 
 const handler = NextAuth({
-  pages:{
-    signIn: "/Login"
+  pages: {
+    signIn: '/Login'
   },
   providers: [
     CredentialsProvider({
-      name: "Credentials",
-      credentials:{
-        username: {}, 
-        password: {},
+      name: 'Credentials',
+      credentials: {
+        username: {},
+        password: {}
       },
 
       async authorize(credentials) {
-        if(!credentials) return null;
+        if (!credentials) return null;
+
         try {
-          const response = await fetch("http://localhost:8080/auth/login", {
-            method: 'POST',
-            headers: {"content-type": "application/json"},
-            body: JSON.stringify({
-              username: credentials.username,
-              password: credentials.password,
-            }),
+          const { data, success } = await login({
+            username: credentials.username,
+            password: credentials.password
           });
-          console.log("route", credentials.username, credentials.password)
-          if(response.status !== 200) return null;
-          const authData:AuthData = await response.json();
-          if(!authData.accessToken) return null;
-          (await cookies()).set("accessToken", authData.accessToken);
+
+          if (!success) return null;
+
+          await setToken(data.access_token);
+
           return {
-            accessToken: authData.accessToken,
-          }
-        } catch(e) {
-          console.log(e);
-          return;
-        };
+            id: data.user_id,
+            name: data.user_name
+          };
+        } catch (e) {
+          return null;
+        }
       }
     })
   ]
-})
+});
 
-export {handler as GET, handler as POST};
+async function setToken(token: string) {
+  (await cookies()).set('access_token', token);
+}
+
+export { handler as GET, handler as POST };
