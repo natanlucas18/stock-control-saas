6a83c83f36ec089b1e2781dae8f80d3c77f80e2c
alissonasj <alissonsilva.jesus@gmail.com>
2025-09-29 22:59:01 -0300

    melhora tipagem do DTO e types e adiciona funcao `getToken()`

---
+++ frontend/src/lib/get-token.ts
@@ -0,0 +1,7 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+export async function getToken() {
+  return (await cookies()).get('access_token')?.value || '';
+}
--- frontend/src/types/server-dto.ts
+++ frontend/src/types/server-dto.ts
@@ -1,10 +1,27 @@
+export type PaginationOptions = {
+  pageNumber: number;
+  pageSize: number;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  first: boolean;
+};
+
+type error = {
+  timestamp: string;
+  status: number;
+  message: string;
+  path: string;
+};
+
 export type ServerDTO<T = null> = {
   data: T;
-  error: {
-    timestamp: string;
-    status: number;
-    message: string;
-    path: string;
-  } | null;
+  errors: error[] | null;
+  success: boolean;
+};
+
+export type ServerDTOArray<T = null> = {
+  data: { content: T[]; pagination: PaginationOptions };
+  errors: error[] | null;
   success: boolean;
 };
