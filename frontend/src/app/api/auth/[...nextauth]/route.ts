181f5730c7d54c4ded6fe02600ee5b7dee552d7d
natanlucas18 <natanlucaslg1839@gmail.com>
2026-02-03 19:06:48 -0300

    implementa lógica para salvar dados do usuário e token na session e no jwt do nextauth

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,16 @@
 import { login } from '@/services/login-service';
 import { PathLinks } from '@/types/path-links';
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { cookies } from 'next/headers';
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   pages: {
     signIn: PathLinks.SIGN_IN
+  },
+  session: {
+    strategy: 'jwt' as const,
+    maxAge: 30 * 24 * 60 * 60 // 30 dias
   },
   providers: [
     CredentialsProvider({
@@ -27,25 +31,54 @@ const handler = NextAuth({
 
           if (!success) return null;
 
-          await setCookie('accessToken', data.accessToken);
-          await setCookie('userRoles', JSON.stringify(data.userRoles));
+          (await cookies()).set('userRoles', data.userRoles.toString(), {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            path: '/dashboard'
+          })
 
           return {
             id: data.userId,
             name: data.userName,
             accessToken: data.accessToken,
-            userRoles: data.userRoles
+            userRoles: data.userRoles,
+            expiresAt: data.expiresAt
           };
         } catch {
           return null;
         }
       }
     })
-  ]
-});
+  ],
+    callbacks: {
+    async jwt({ user, token }) {
+      if(user) {
+      token.sub = user.id;
+      token.name = user.name;
+      token.email = user.email;
+      token.accessToken = user.accessToken;
+      token.userRoles = user.userRoles;
+      token.expiresAt = new Date(user.expiresAt).getTime()
+      };
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+      session.user.id = token.sub as string;
+      session.user.name = token.name;
+      session.user.email = token.email;
+      session.user.accessToken = token.accessToken as string;
+      session.user.userRoles = token.userRoles;
+      session.expires = new Date(token.expiresAt).toISOString()
+      }
+      return session;
+    }
+  }
 
-async function setCookie(key: string, value: string) {
-  (await cookies()).set(key, value);
-}
 
+};
+
+
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST };
