3b0f0a97c1c12abca74331f3a46fb256da7be448
natanlucas18 <natanlucaslg1839@gmail.com>
2026-02-03 19:03:04 -0300

    fix: corrige retorno da lib GetApiUrl

--- frontend/src/lib/api-url.ts
+++ frontend/src/lib/api-url.ts
@@ -1,9 +1,3 @@
 export function getApiUrl() {
-  // Se estivermos no navegador, usamos a URL pública
-  if (typeof window !== 'undefined') {
     return process.env.NEXT_PUBLIC_API_URL_CLIENT;
-  }
-
-  // Se estivermos no servidor (Docker), usamos a URL interna
-  return process.env.INTERNAL_API_URL;
 }
