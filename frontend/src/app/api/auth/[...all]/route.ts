248b1006834dc2d24b23c0d564e828f4be8bbc54
natanlucas18 <natanlucaslg1839@gmail.com>
2026-03-29 16:20:42 -0300

    implementa route do better auth

---
+++ frontend/src/app/api/auth/[...all]/route.ts
@@ -0,0 +1,7 @@
+import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
+
+const handler = toNextJsHandler(auth.handler);
+
+export const GET = handler;
+export const POST = handler;
