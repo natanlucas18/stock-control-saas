fd91a9a538656d6e008e92152f25352a15d1be3d
natanlucas18 <natanlucaslg1839@gmail.com>
2026-03-29 16:22:25 -0300

    adiciona authClient para uso em componentes do lado do servidor

---
+++ frontend/src/lib/auth-client.ts
@@ -0,0 +1,7 @@
+import { createAuthClient } from "better-auth/client";
+
+export const authClient = createAuthClient({
+    baseURL: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth`
+});
+
+export const { signIn, signOut, useSession } = authClient;
