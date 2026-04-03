2521fd57e762eaa767f9545e4031710532ae237b
Nathangc77 <nathangc7@gmail.com>
2026-04-03 18:53:08 -0300

    feat: adiciona set cookies do HttpOnly

--- backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/AuthController.java
+++ backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.hextech.estoque_api.application.services.AuthService;
 import com.hextech.estoque_api.interfaces.controllers.docs.AuthControllerDocs;
 import com.hextech.estoque_api.interfaces.dtos.StarndardResponse.StandardResponse;
 import com.hextech.estoque_api.interfaces.dtos.security.AccountCredentialsDTO;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,24 +21,40 @@ public class AuthController implements AuthControllerDocs {
     private AuthService service;
 
     @PostMapping("/login")
-    public ResponseEntity<StandardResponse<?>> login(@RequestBody @Valid AccountCredentialsDTO credentials) {
+    public ResponseEntity<StandardResponse<?>> login(@RequestBody @Valid AccountCredentialsDTO credentials,
+                                                     HttpServletResponse response) {
         var token = service.login(credentials);
+
+        setCookies(response, token.getAccessToken(), token.getRefreshToken());
+
         return ResponseEntity.ok().body(new StandardResponse<>(true, token));
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<StandardResponse<?>> refreshToken(@RequestBody JsonNode body,
-                                                            @RequestHeader("Authorization") String refreshToken) {
+                                                            @RequestHeader("Authorization") String refreshToken,
+                                                            HttpServletResponse response) {
         String username = body.get("username").asText();
 
         if (!parametersAreValid(username, refreshToken))
             throw new IllegalArgumentException("Parâmetros do cliente inválidos.");
 
         var token = service.refreshToken(username, refreshToken);
+        setCookies(response, token.getAccessToken(), token.getRefreshToken());
         return ResponseEntity.ok().body(new StandardResponse<>(true, token));
     }
 
     private boolean parametersAreValid(String username, String refreshToken) {
         return StringUtils.isNotBlank(username) || StringUtils.isNotBlank(refreshToken);
+    }
+
+    private void setCookies(HttpServletResponse response, String accessToken, String refreshToken) {
+        String accessCookie = String.format("accessToken=%s; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=%d",
+                accessToken, 60*15);
+        String refreshCookie = String.format("refreshToken=%s; HttpOnly; Secure; SameSite=None; Path=/auth/refresh; Max-Age=%d",
+                refreshToken, 60*60*24*7);
+
+        response.addHeader("Set-Cookie", accessCookie);
+        response.addHeader("Set-Cookie", refreshCookie);
     }
 }
--- backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/docs/AuthControllerDocs.java
+++ backend/src/main/java/com/hextech/estoque_api/interfaces/controllers/docs/AuthControllerDocs.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "Autenticação", description = "Operações de Autenticação")
@@ -19,12 +20,12 @@ public interface AuthControllerDocs {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = StandardResponse.class))),
             @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
     })
-    ResponseEntity<StandardResponse<?>> login(AccountCredentialsDTO credentials);
+    ResponseEntity<StandardResponse<?>> login(AccountCredentialsDTO credentials, HttpServletResponse response);
 
     @Operation(summary = "Realiza o refresh de um token")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = StandardResponse.class))),
             @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
     })
-    ResponseEntity<StandardResponse<?>> refreshToken(JsonNode body, String refreshToken);
+    ResponseEntity<StandardResponse<?>> refreshToken(JsonNode body, String refreshToken, HttpServletResponse response);
 }
