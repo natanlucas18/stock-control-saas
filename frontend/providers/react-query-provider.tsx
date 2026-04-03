8e2f51868d9b70b1cf1abb8e12575d5c2e3c9659
natanlucas18 <natanlucaslg1839@gmail.com>
2026-04-03 19:41:27 -0300

    implementa provider do react query

---
+++ frontend/providers/react-query-provider.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode, useState } from 'react';
+
+export function ReactQueryProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false
+      }
+    }
+  }));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
