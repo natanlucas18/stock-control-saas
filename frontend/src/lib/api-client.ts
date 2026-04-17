65c6ffd1315e768802e37441fe1c08f8ac01280c
natanlucas18 <natanlucaslg1839@gmail.com>
2026-04-17 10:11:57 -0300

    implementa refresh token para reagir ao 401 da API

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -1,6 +1,8 @@
 "use client";
 
-export async function apiFetch<T>(
+import { getApiUrl } from "./api-url";
+
+/*export async function apiFetch<T>(
   url: string,
   options?: RequestInit,
 ): Promise<T> {
@@ -13,5 +15,62 @@ export async function apiFetch<T>(
     },
   });
   return response.json();
+}*/
+
+let isRefreshing = false
+let refreshPromise: Promise<void> | null = null
+const localhost = getApiUrl()
+
+async function refreshToken() {
+  await fetch(`${localhost}/auth/refresh`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
 }
+
+export async function apiFetch<T>(
+  url: string,
+  options?: RequestInit,
+): Promise<T> {
+  const makeRequest = () =>
+    fetch(url, {
+      ...options,
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        ...(options?.headers || {}),
+      },
+    })
+
+  let response = await makeRequest()
+
+  if (response.status !== 401) {
+    if (!response.ok) throw new Error(await response.text())
+    return response.json()
+  }
+
+  try {
+    if (!isRefreshing) {
+      isRefreshing = true
+      refreshPromise = refreshToken()
+      await refreshPromise
+      isRefreshing = false
+    } else {
+      await refreshPromise
+    }
+
+    response = await makeRequest()
+
+    if (!response.ok) throw new Error(await response.text())
+
+    return response.json()
+  } catch (error) {
+    window.location.href = `/dashboard/login`
+    throw error
+  }
+}
+
 
