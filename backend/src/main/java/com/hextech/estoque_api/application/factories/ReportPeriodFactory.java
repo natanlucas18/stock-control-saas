1d28346201e13f23a40eb3b36e1707907c2539c9
nathan <nathangc7@gmail.com>
2026-04-29 16:20:59 -0300

    refactor: ajusta validacao de datas dos relatorios

--- backend/src/main/java/com/hextech/estoque_api/application/factories/ReportPeriodFactory.java
+++ backend/src/main/java/com/hextech/estoque_api/application/factories/ReportPeriodFactory.java
@@ -1,6 +1,7 @@
 package com.hextech.estoque_api.application.factories;
 
 import com.hextech.estoque_api.domain.entities.ReportPeriod;
+import com.hextech.estoque_api.domain.exceptions.BusinessException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,10 +14,7 @@ public class ReportPeriodFactory {
 
         try {
             LocalDate end = (endDate.isEmpty() || endDate.equals("null")) ? now : LocalDate.parse(endDate);
-            LocalDate start = (startDate.isEmpty() || startDate.equals("null")) ?
-                    LocalDate.of(end.getYear(), end.getMonth().minus(1), end.getDayOfMonth()) : LocalDate.parse(startDate);
-
-            if (start.isAfter(end)) throw new IllegalArgumentException("Data inicial não pode ser posterior a data final.");
+            LocalDate start = getLocalDate(startDate, end, now);
 
             return new ReportPeriod(
                     LocalDateTime.of(start.getYear(), start.getMonth(), start.getDayOfMonth(), 0, 0),
@@ -25,5 +23,17 @@ public class ReportPeriodFactory {
         } catch (DateTimeParseException e) {
             throw new IllegalArgumentException("Datas inválidas. Use o formato yyyy-MM-dd.");
         }
+    }
+
+    private static LocalDate getLocalDate(String startDate, LocalDate end, LocalDate now) {
+        LocalDate start = (startDate.isEmpty() || startDate.equals("null")) ?
+                end.minusMonths(1) : LocalDate.parse(startDate);
+
+        if (start.isAfter(end)) throw new BusinessException("Data inicial não pode ser posterior a data final.");
+
+        if (start.isAfter(now) || end.isAfter(now)) throw new BusinessException("Datas não podem ser maiores que a data atual.");
+
+        if (start.plusYears(1).isBefore(end)) throw new BusinessException("Período não pode ser maior que 1 ano.");
+        return start;
     }
 }
